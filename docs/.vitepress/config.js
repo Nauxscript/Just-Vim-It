468a833838a3868836153f944745002140ea8adf
nauxscript <nauxscript@gmail.com>
2022-06-07 11:07:54 +0800

    use vitepress to build page and add day 6

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: "Just Vim It",
+  description: "A simple vim tutorial for who haven't learn \ use \ know about vim",
+  lastUpdated: true,
+  themeConfig: {
+    siteTitle: "Just Vim It",
+    nav: [
+      { text: 'About Nauxscript', link: 'https://www.nauxscript.com' },
+      { text: 'Github', link: 'https://github.com/Nauxscript/Just-Vim-It' }
+    ],
+    sidebar: [
+      {
+        text: "介绍",
+        items: [
+          { text: "写在前面", link: "/index.md" }
+        ]
+      },
+      {
+        text: '训练',
+        items: [
+          { text: 'Day 1', link: '/day-1.md' },
+          { text: 'Day 2', link: '/day-2.md' },
+          { text: 'Day 3', link: '/day-3.md' },
+          { text: 'Day 4', link: '/day-4.md' },
+          { text: 'Day 5', link: '/day-5.md' },
+          { text: 'Rest Day 1', link: '/rest-dy-1.md' },
+          { text: 'Day 6', link: '/day-6.md' },
+        ]
+      }
+    ],
+    lastUpdatedText: "Last updated" 
+  },
+}
