1e6e86a0e0d65d18448f00b3f2aecc48abeb35e8
nauxscript mac-mini <nauxscript@gmail.com>
2022-06-12 22:18:49 +0800

    add day 10

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -30,6 +30,7 @@ module.exports = {
           { text: 'Day 7', link: '/day-7.md' },
           { text: 'Day 8', link: '/day-8.md' },
           { text: 'Day 9', link: '/day-9.md' },
+          { text: 'Day 10', link: '/day-10.md' },
         ]
       }
     ],
